e1ed216e87e54963a42b2736bde80bee9dabb950
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-10 22:02:32 +0530

    js function created

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
 
 
-app.listen(5000,()=>{
-    console.log("connected to server");
-});
+// app.listen(5000,()=>{
+//     console.log("connected to server");
+// });
 
 //javascript tutorial
 
 //datatypes
 
 // var ,let and const
+
+
+
+function userDetails(email, name, username) {
+    console.log(`details are ${email},${name},${username}`);
+}
+
+
+userDetails("ankitbarai641@gmail.com","Ankit Barai","Anni")
