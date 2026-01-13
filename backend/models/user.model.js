3d1b1d2106ad54f7347e3f57b265e7fa617840d3
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-13 21:19:23 +0530

    commented backend

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,34 +1,35 @@
-const mongoose = require("mongoose");
-const db = require('./config');
-const bcrypt = require('bcrypt');
+// const mongoose = require("mongoose");
+// const db = require('./config');
+// const bcrypt = require('bcrypt');
 
-const { Schema } = mongoose;
+// const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    email:{
-        type:String,
-        lowercase:true,
-        unique:true,
-        required:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    }
-})
+// const userSchema = new Schema({
+//     email:{
+//         type:String,
+//         lowercase:true,
+//         unique:true,
+//         required:true,
+//     },
+//     password:{
+//         type:String,
+//         required:true,
+//     }
+// })
 
-userSchema.pre('save',async function(){
-    try {
-        var user = this;
-        const salt = await(bcrypt.genSalt(10));
-        const hashPass = await(bcrypt.hash(user.password,salt));
-        user.password = hashPass;
-    } catch (error) {
-        throw error;
-    }
-});
+// userSchema.pre('save',async function(){
+//     try {
+//         var user = this;
+//         const salt = await(bcrypt.genSalt(10));
+//         const hashPass = await(bcrypt.hash(user.password,salt));
+//         user.password = hashPass;
+//     } catch (error) {
+//         throw error;
+//     }
+// });
 
-const userModel = db.model('user',userSchema); //user is table name in mongoDB
+// const userModel = db.model('user',userSchema); //user is table name in mongoDB
 
 
-module.exports = userModel;
+// module.exports = userModel;
+
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,8 @@
-const router = require('express').Router();
+// const router = require('express').Router();
 
-const userController = require('../controllers/user.controller');
+// const userController = require('../controllers/user.controller');
 
-router.post('/registration',userController);
+// router.post('/registration',userController);
 
-module.exports = router;
+// module.exports = router;
+
