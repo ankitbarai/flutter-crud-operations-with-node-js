d37e2079de75c129b550b2d91b9e5fce8cd2744e
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-13 21:17:57 +0530

    homescereen created

---
+++ lib/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CRUD Ops'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+
+          },
+           child: Text('CREATE')),
+           ElevatedButton(onPressed: (){
+
+          },
+           child: Text('READ')),
+           ElevatedButton(onPressed: (){
+
+          },
+           child: Text('UPDATE')),
+           ElevatedButton(onPressed: (){
+
+          },
+           child: Text('DELETE')),
+        ],
+      ),
+    );
+  }
+}
